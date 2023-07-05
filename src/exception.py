9fa90604ba6e0891e5c9d8ea4639e05940460fb3
venkatesh-kunchala <kunchalavenkatesh06@gmail.com>
2023-07-05 12:48:12 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -9,11 +9,13 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
-class CustomException(Exception):
-    def __init__(self,error_message,error_details:sys):
-        super().__init__(error_message)
-        self.error_message=error_message
+    
 
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
     def __str__(self):
         return self.error_message
     
